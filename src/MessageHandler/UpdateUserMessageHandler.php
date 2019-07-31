46d4266a9a114127412f22ebc750f0bb7e2544cd
Roger <pengsongyang@cqxiaokang.com>
2019-07-31 16:52:31 +0800

    update data

--- src/MessageHandler/UpdateUserMessageHandler.php
+++ src/MessageHandler/UpdateUserMessageHandler.php
@@ -6,6 +6,7 @@ namespace App\MessageHandler;
 
 use App\Message\UpdateUserMessage;
 use App\Service\UserService;
+use App\Tools\GetRandTools;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class UpdateUserMessageHandler implements MessageHandlerInterface
@@ -24,8 +25,10 @@ class UpdateUserMessageHandler implements MessageHandlerInterface
     {
         $user = $this->userService->getUserInfoById($message->getId());
 
+        $user->setSex(GetRandTools::getRandSex());
+        $user->setIdCard(GetRandTools::getRandIdCard());
+        $user->setGold(GetRandTools::getRandFloat(1, 99));
         $user->setBirthday(new \DateTime('2015-01-01'));
-
 
         $this->userService->saveUser($user);
 
